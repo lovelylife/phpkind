cb082e01f3e33faf8c12a623b8903a09542eca8e
Q <life.qm@gmail.com>
2013-11-12 08:31:49 -0800

    construct project

---
+++ phpweibosdk/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '59191755' );
+define( "WB_SKEY" , '143ffeb9cb1838b0b4ac57e52881021d' );
+define( "WB_CALLBACK_URL" , 'http://wayixia.com/phpweibosdk/callback.php' );

8546f8474de6344b6e6b64e7a472df64f3078cbd
Q <life.qm@gmail.com>
2014-04-29 23:47:10 +0800

    modif template

--- qlib/ui/calendar.js
+++ qlib/ui/calendar.js
@@ -13,164 +13,166 @@ var lastcheckedyear = false;
 var lastcheckedmonth = false;
 
 function _cancelBubble(event) {
-	e = event ? event : window.event ;
-	if(document.all) {
-		e.cancelBubble = true;
-	} else {
-		e.stopPropagation();
-	}
+  e = event ? event : window.event ;
+  if(document.all) {
+    e.cancelBubble = true;
+  } else {
+    e.stopPropagation();
+  }
 }
 
 function loadcalendar() {
-	s = '';
-	s += '<div id="calendar" style="display:none; position:absolute; z-index:9;" onclick="_cancelBubble(event)">';
-	if (document.all)
-	{
-		s += '<iframe width="200" height="160" src="about:blank" style="position: absolute;z-index:-1;"></iframe>';
-	}
-	s += '<div style="width: 200px;"><table class="tableborder" cellspacing="0" cellpadding="0" width="100%" style="text-align: center">';
-	s += '<tr align="center" class="header"><td class="header"><a href="#" onclick="refreshcalendar(yy, mm-1);return false" title="上一月">&lt;&lt;</a></td><td colspan="5" style="text-align: center" class="header"><a href="#" onclick="showdiv(\'year\');_cancelBubble(event);return false" title="点击选择年份" id="year"></a>&nbsp;/ &nbsp;<a id="month" title="点击选择月份" href="#" onclick="showdiv(\'month\');_cancelBubble(event);return false"></a></td><td class="header"><A href="#" onclick="refreshcalendar(yy, mm+1);return false" title="下一月">&gt;&gt;</A></td></tr>';
-	s += '<tr class="category"><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
-	for(var i = 0; i < 6; i++) {
-		s += '<tr class="altbg2">';
-		for(var j = 1; j <= 7; j++)
-			s += "<td id=d" + (i * 7 + j) + " height=\"19\">0</td>";
-		s += "</tr>";
-	}
-	s += '<tr id="hourminute"><td colspan="7" align="center"><input type="text" size="1" value="" id="hour" onKeyUp=\'this.value=this.value > 23 ? 23 : zerofill(this.value);controlid.value=controlid.value.replace(/\\d+(\:\\d+)/ig, this.value+"$1")\'> 点 <input type="text" size="1" value="" id="minute" onKeyUp=\'this.value=this.value > 59 ? 59 : zerofill(this.value);controlid.value=controlid.value.replace(/(\\d+\:)\\d+/ig, "$1"+this.value)\'> 分</td></tr>';
-	s += '</table></div></div>';
-	s += '<div id="calendar_year" onclick="_cancelBubble(event)"><div class="col">';
-	for(var k = 1930; k <= 2019; k++) {
-		s += k != 1930 && k % 10 == 0 ? '</div><div class="col">' : '';
-		s += '<a href="#" onclick="refreshcalendar(' + k + ', mm);$(\'calendar_year\').style.display=\'none\';return false"><span' + (today.getFullYear() == k ? ' class="today"' : '') + ' id="calendar_year_' + k + '">' + k + '</span></a><br />';
-	}
-	s += '</div></div>';
-	s += '<div id="calendar_month" onclick="_cancelBubble(event)">';
-	for(var k = 1; k <= 12; k++) {
-		s += '<a href="#" onclick="refreshcalendar(yy, ' + (k - 1) + ');$(\'calendar_month\').style.display=\'none\';return false"><span' + (today.getMonth()+1 == k ? ' class="today"' : '') + ' id="calendar_month_' + k + '">' + k + ( k < 10 ? '&nbsp;' : '') + ' 月</span></a><br />';
-	}
-	s += '</div>';
+  s = '';
+  s += '<div id="calendar" style="display:none; position:absolute; z-index:9;" onclick="_cancelBubble(event)">';
+  if (document.all)
+  {
+    s += '<iframe width="200" height="160" src="about:blank" style="position: absolute;z-index:-1;"></iframe>';
+  }
+  s += '<div style="width: 200px;"><table class="tableborder" cellspacing="0" cellpadding="0" width="100%" style="text-align: center">';
+  s += '<tr align="center" class="header"><td class="header"><a href="#" onclick="refreshcalendar(yy, mm-1);return false" title="上一月">&lt;&lt;</a></td><td colspan="5" style="text-align: center" class="header"><a href="#" onclick="showdiv(\'year\');_cancelBubble(event);return false" title="点击选择年份" id="year"></a>&nbsp;/ &nbsp;<a id="month" title="点击选择月份" href="#" onclick="showdiv(\'month\');_cancelBubble(event);return false"></a></td><td class="header"><A href="#" onclick="refreshcalendar(yy, mm+1);return false" title="下一月">&gt;&gt;</A></td></tr>';
+  s += '<tr class="category"><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
+  for(var i = 0; i < 6; i++) {
+    s += '<tr class="altbg2">';
+    for(var j = 1; j <= 7; j++)
+      s += "<td id=d" + (i * 7 + j) + " height=\"19\">0</td>";
+    s += "</tr>";
+  }
+  s += '<tr id="hourminute"><td colspan="7" align="center"><input type="text" size="1" value="" id="hour" onKeyUp=\'this.value=this.value > 23 ? 23 : zerofill(this.value);controlid.value=controlid.value.replace(/\\d+(\:\\d+)/ig, this.value+"$1")\'> 点 <input type="text" size="1" value="" id="minute" onKeyUp=\'this.value=this.value > 59 ? 59 : zerofill(this.value);controlid.value=controlid.value.replace(/(\\d+\:)\\d+/ig, "$1"+this.value)\'> 分</td></tr>';
+  s += '</table></div></div>';
+  s += '<div id="calendar_year" onclick="_cancelBubble(event)"><div class="col">';
+  for(var k = 1930; k <= 2019; k++) {
+    s += k != 1930 && k % 10 == 0 ? '</div><div class="col">' : '';
+    s += '<a href="#" onclick="refreshcalendar(' + k + ', mm);$(\'calendar_year\').style.display=\'none\';return false"><span' + (today.getFullYear() == k ? ' class="today"' : '') + ' id="calendar_year_' + k + '">' + k + '</span></a><br />';
+  }
+  s += '</div></div>';
+  s += '<div id="calendar_month" onclick="_cancelBubble(event)">';
+  for(var k = 1; k <= 12; k++) {
+    s += '<a href="#" onclick="refreshcalendar(yy, ' + (k - 1) + ');$(\'calendar_month\').style.display=\'none\';return false"><span' + (today.getMonth()+1 == k ? ' class="today"' : '') + ' id="calendar_month_' + k + '">' + k + ( k < 10 ? '&nbsp;' : '') + ' 月</span></a><br />';
+  }
+  s += '</div>';
 
-	var nElement = document.createElement("div");
-	nElement.innerHTML=s;
-	document.getElementsByTagName("body")[0].appendChild(nElement);
+  var nElement = document.createElement("div");
+  nElement.innerHTML=s;
+  document.getElementsByTagName("body")[0].appendChild(nElement);
 
-//	document.write(s);
-	document.onclick = function(event) {
-		Q.$('calendar').style.display = 'none';
-		Q.$('calendar_year').style.display = 'none';
-		Q.$('calendar_month').style.display = 'none';
-	}
-	Q.$('calendar').onclick = function(event) {
-		_cancelBubble(event);
-		Q.$('calendar_year').style.display = 'none';
-		Q.$('calendar_month').style.display = 'none';
-	}
+//  document.write(s);
+  document.onclick = function(event) {
+    Q.$('calendar').style.display = 'none';
+    Q.$('calendar_year').style.display = 'none';
+    Q.$('calendar_month').style.display = 'none';
+  }
+  Q.$('calendar').onclick = function(event) {
+    _cancelBubble(event);
+    Q.$('calendar_year').style.display = 'none';
+    Q.$('calendar_month').style.display = 'none';
+  }
 }
 
 function parsedate(s) {
-	/(\d+)\-(\d+)\-(\d+)\s*(\d*):?(\d*)/.exec(s);
-	var m1 = (RegExp.$1 && RegExp.$1 > 1899 && RegExp.$1 < 2101) ? parseFloat(RegExp.$1) : today.getFullYear();
-	var m2 = (RegExp.$2 && (RegExp.$2 > 0 && RegExp.$2 < 13)) ? parseFloat(RegExp.$2) : today.getMonth() + 1;
-	var m3 = (RegExp.$3 && (RegExp.$3 > 0 && RegExp.$3 < 32)) ? parseFloat(RegExp.$3) : today.getDate();
-	var m4 = (RegExp.$4 && (RegExp.$4 > -1 && RegExp.$4 < 24)) ? parseFloat(RegExp.$4) : 0;
-	var m5 = (RegExp.$5 && (RegExp.$5 > -1 && RegExp.$5 < 60)) ? parseFloat(RegExp.$5) : 0;
-	/(\d+)\-(\d+)\-(\d+)\s*(\d*):?(\d*)/.exec("0000-00-00 00\:00");
-	return new Date(m1, m2 - 1, m3, m4, m5);
+  /(\d+)\-(\d+)\-(\d+)\s*(\d*):?(\d*)/.exec(s);
+  var m1 = (RegExp.$1 && RegExp.$1 > 1899 && RegExp.$1 < 2101) ? parseFloat(RegExp.$1) : today.getFullYear();
+  var m2 = (RegExp.$2 && (RegExp.$2 > 0 && RegExp.$2 < 13)) ? parseFloat(RegExp.$2) : today.getMonth() + 1;
+  var m3 = (RegExp.$3 && (RegExp.$3 > 0 && RegExp.$3 < 32)) ? parseFloat(RegExp.$3) : today.getDate();
+  var m4 = (RegExp.$4 && (RegExp.$4 > -1 && RegExp.$4 < 24)) ? parseFloat(RegExp.$4) : 0;
+  var m5 = (RegExp.$5 && (RegExp.$5 > -1 && RegExp.$5 < 60)) ? parseFloat(RegExp.$5) : 0;
+  /(\d+)\-(\d+)\-(\d+)\s*(\d*):?(\d*)/.exec("0000-00-00 00\:00");
+  return new Date(m1, m2 - 1, m3, m4, m5);
 }
 
 function settime(d) {
-	Q.$('calendar').style.display = 'none';
-	controlid.value = yy + "-" + zerofill(mm + 1) + "-" + zerofill(d) + (addtime ? ' ' + zerofill(Q.$('hour').value) + ':' + zerofill(Q.$('minute').value) : '');
+  Q.$('calendar').style.display = 'none';
+  controlid.value = yy + "-" + zerofill(mm + 1) + "-" + zerofill(d) + (addtime ? ' ' + zerofill(Q.$('hour').value) + ':' + zerofill(Q.$('minute').value) : '');
 }
 
 function showcalendar(event, controlid1, addtime1, startdate1, enddate1) {
-	controlid = controlid1;
-	addtime = addtime1;
-	startdate = startdate1 ? parsedate(startdate1) : false;
-	enddate = enddate1 ? parsedate(enddate1) : false;
-	currday = controlid.value ? parsedate(controlid.value) : today;
-	hh = currday.getHours();
-	ii = currday.getMinutes();
-	var p = Q.absPosition(Q.$(controlid));
-	Q.$('calendar').style.display = 'block';
-	Q.$('calendar').style.left = (p.left+p.width+1)+'px';
-	Q.$('calendar').style.top	= (p.top+2)+'px';
-	_cancelBubble(event);
-	refreshcalendar(currday.getFullYear(), currday.getMonth());
-	if(lastcheckedyear != false) {
-		Q.$('calendar_year_' + lastcheckedyear).className = 'default';
-		Q.$('calendar_year_' + today.getFullYear()).className = 'today';
-	}
-	if(lastcheckedmonth != false) {
-		Q.$('calendar_month_' + lastcheckedmonth).className = 'default';
-		Q.$('calendar_month_' + (today.getMonth() + 1)).className = 'today';
-	}
-	Q.$('calendar_year_' + currday.getFullYear()).className = 'checked';
-	Q.$('calendar_month_' + (currday.getMonth() + 1)).className = 'checked';
-	Q.$('hourminute').style.display = addtime ? '' : 'none';
-	lastcheckedyear = currday.getFullYear();
-	lastcheckedmonth = currday.getMonth() + 1;
+  controlid = controlid1;
+  addtime = addtime1;
+  startdate = startdate1 ? parsedate(startdate1) : false;
+  enddate = enddate1 ? parsedate(enddate1) : false;
+  currday = controlid.value ? parsedate(controlid.value) : today;
+  hh = currday.getHours();
+  ii = currday.getMinutes();
+  var p = Q.absPosition(Q.$(controlid));
+  Q.$('calendar').style.display = 'block';
+  Q.$('calendar').style.left = (p.left+p.width+1)+'px';
+  Q.$('calendar').style.top  = (p.top+2)+'px';
+  _cancelBubble(event);
+  refreshcalendar(currday.getFullYear(), currday.getMonth());
+  if(lastcheckedyear != false) {
+    Q.$('calendar_year_' + lastcheckedyear).className = 'default';
+    Q.$('calendar_year_' + today.getFullYear()).className = 'today';
+  }
+  if(lastcheckedmonth != false) {
+    Q.$('calendar_month_' + lastcheckedmonth).className = 'default';
+    Q.$('calendar_month_' + (today.getMonth() + 1)).className = 'today';
+  }
+  Q.$('calendar_year_' + currday.getFullYear()).className = 'checked';
+  Q.$('calendar_month_' + (currday.getMonth() + 1)).className = 'checked';
+  Q.$('hourminute').style.display = addtime ? '' : 'none';
+  lastcheckedyear = currday.getFullYear();
+  lastcheckedmonth = currday.getMonth() + 1;
 }
 
 function refreshcalendar(y, m) {
-	var x = new Date(y, m, 1);
-	var mv = x.getDay();
-	var d = x.getDate();
-	var dd = null;
-	yy = x.getFullYear();
-	mm = x.getMonth();
-	$("year").innerHTML = yy;
-	$("month").innerHTML = mm + 1 > 9  ? (mm + 1) : '0' + (mm + 1);
+  var x = new Date(y, m, 1);
+  var mv = x.getDay();
+  var d = x.getDate();
+  var dd = null;
+  yy = x.getFullYear();
+  mm = x.getMonth();
+  $("year").innerHTML = yy;
+  $("month").innerHTML = mm + 1 > 9  ? (mm + 1) : '0' + (mm + 1);
 
-	for(var i = 1; i <= mv; i++) {
-		dd = $("d" + i);
-		dd.innerHTML = "&nbsp;";
-		dd.className = "";
-	}
+  for(var i = 1; i <= mv; i++) {
+    dd = $("d" + i);
+    dd.innerHTML = "&nbsp;";
+    dd.className = "";
+  }
 
-	while(x.getMonth() == mm) {
-		dd = $("d" + (d + mv));
-		dd.innerHTML = '<a href="###" onclick="settime(' + d + ');return false">' + d + '</a>';
-		if(x.getTime() < today.getTime() || (enddate && x.getTime() > enddate.getTime()) || (startdate && x.getTime() < startdate.getTime())) {
-			dd.className = 'expire';
-		} else {
-			dd.className = 'default';
-		}
-		if(x.getFullYear() == today.getFullYear() && x.getMonth() == today.getMonth() && x.getDate() == today.getDate()) {
-			dd.className = 'today';
-			dd.firstChild.title = '今天';
-		}
-		if(x.getFullYear() == currday.getFullYear() && x.getMonth() == currday.getMonth() && x.getDate() == currday.getDate()) {
-			dd.className = 'checked';
-		}
-		x.setDate(++d);
-	}
+  while(x.getMonth() == mm) {
+    dd = $("d" + (d + mv));
+    dd.innerHTML = '<a href="###" onclick="settime(' + d + ');return false">' + d + '</a>';
+    if(x.getTime() < today.getTime() || (enddate && x.getTime() > enddate.getTime()) || (startdate && x.getTime() < startdate.getTime())) {
+      dd.className = 'expire';
+    } else {
+      dd.className = 'default';
+    }
+    if(x.getFullYear() == today.getFullYear() && x.getMonth() == today.getMonth() && x.getDate() == today.getDate()) {
+      dd.className = 'today';
+      dd.firstChild.title = '今天';
+    }
+    if(x.getFullYear() == currday.getFullYear() && x.getMonth() == currday.getMonth() && x.getDate() == currday.getDate()) {
+      dd.className = 'checked';
+    }
+    x.setDate(++d);
+  }
 
-	while(d + mv <= 42) {
-		dd = $("d" + (d + mv));
-		dd.innerHTML = "&nbsp;";
-		d++;
-	}
+  while(d + mv <= 42) {
+    dd = $("d" + (d + mv));
+    dd.innerHTML = "&nbsp;";
+    d++;
+  }
 
-	if(addtime) {
-		Q.$('hour').value = zerofill(hh);
-		Q.$('minute').value = zerofill(ii);
-	}
+  if(addtime) {
+    Q.$('hour').value = zerofill(hh);
+    Q.$('minute').value = zerofill(ii);
+  }
 }
 
 function showdiv(id) {
 
-	var p = Q.absPosition(Q.$(id));
-	Q.$('calendar_' + id).style.left = p.left+'px';
-	Q.$('calendar_' + id).style.top = (p.top + 16)+'px';
-	Q.$('calendar_' + id).style.display = 'block';
+  var p = Q.absPosition(Q.$(id));
+  Q.$('calendar_' + id).style.left = p.left+'px';
+  Q.$('calendar_' + id).style.top = (p.top + 16)+'px';
+  Q.$('calendar_' + id).style.display = 'block';
 }
 
 function zerofill(s) {
-	var s = parseFloat(s.toString().replace(/(^[\s0]+)|(\s+$)/g, ''));
-	s = isNaN(s) ? 0 : s;
-	return (s < 10 ? '0' : '') + s.toString();
+  var s = parseFloat(s.toString().replace(/(^[\s0]+)|(\s+$)/g, ''));
+  s = isNaN(s) ? 0 : s;
+  return (s < 10 ? '0' : '') + s.toString();
 }
 
-loadcalendar();
+Q.Ready(function() {
+  loadcalendar();
+});
